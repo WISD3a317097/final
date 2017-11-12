45c8fd5042f62af1f8b1e8f0e524380d87b93656
s3a317097 <gm10285@gm.student.ncut.edu.tw>
2017-11-12 22:20:36 +0800

    貨品管理 -3

--- resources/views/shop_management.blade.php
+++ resources/views/shop_management.blade.php
@@ -3,13 +3,25 @@
 @section('content')
 <h1>貨物管理</h1>
     <div class='row placeholders '>
-       <div class='col'>
-            
-       </div>
-       <div class='col'>
-            
-       </div>
-       <div class='col'>
-            
-       </div>
+        <div class='col'>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
