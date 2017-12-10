443bdc8bb1ed4e54cf4f20496e1b8f5e7295e2bd
s3a317097 <gm10285@gm.student.ncut.edu.tw>
2017-12-11 01:28:10 +0800

    bug_fix

--- app/Http/Controllers/BuyController.php
+++ app/Http/Controllers/BuyController.php
@@ -34,14 +34,16 @@ class BuyController extends Controller
             }
             foreach($flow->orderlist as $o){
                 $order[]=array('total_money'=>$o->total_money,'service'=>$o->reserve,'time'=>$o->time);
+                $shop=$o->shop;
             }
             $lists=$lists::find($id)->get();
             for($i=0;$i<count($lists);$i++){
                 $name=$this->get_foodName($lists[$i]->food_id);
                 $ll[]=array('food'=>$name,'amount'=>$lists[$i]->amount,'money'=>$lists[$i]->money);
-            }    
-        }catch(\Exception $e){
+            }  
             
+        }catch(\Exception $e){
+            //echo $e;
             return response()->json(['success' => '0']);
         }
         return response()->json(['success'=>'1','data'=>$data,'order'=>$order,'food'=>$ll]);
